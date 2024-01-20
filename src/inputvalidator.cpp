21162f1e3188f1f21a446735d2393d97d4d9c329
Kushal Jain <155632770+kushaljain-apra@users.noreply.github.com>
2024-01-21 02:02:40 +0530

    Create inputvalidator.cpp

---
+++ src/inputvalidator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include "inputvalidator.h"
+
+std::string InputValidator::ValidreadString(const std::string& message, const std::string err_message) {
+    std::string userInput;
+    while (true) {
+        std::cout << message;
+        std::getline(std::cin, userInput);
+        if (!userInput.empty()) {
+            break;
+        } else {
+            std::cout << err_message;
+        }
+    }
+    return userInput;
+}
+
+int InputValidator::ValidreadInteger(const std::string& message, const std::string err_message) {
+    int integerValue;
+    std::string userInput;
+
+    while (true) {
+        std::cout << message;
+        std::getline(std::cin, userInput);
+        std::stringstream converter(userInput);
+        if (converter >> integerValue && converter.eof()) {
+            break;
+        } else {
+            std::cout << err_message;
+        }
+    }
+
+    return integerValue;
+}
