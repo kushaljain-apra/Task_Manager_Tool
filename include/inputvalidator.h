990c0019eb4b29f890e6916a5a69ee410ab3ab4b
kushaljain-apra <kushalj@apra.in>
2024-01-22 10:01:32 +0530

    Update inputvalidator.h

--- include/inputvalidator.h
+++ include/inputvalidator.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <string>
+#include <memory>
 
 class InputValidator {
     public:
         std::string ValidreadString(const std::string& message, const std::string err_message);
         int ValidreadInteger(const std::string& message, const std::string err_message);
 };
+
+using InputValidatorptr = std::shared_ptr<InputValidator>;
